3a468e480304df2f443a581d61d53f094812defa
Lesego Ratau <rataulesego95@gmail.com>
2024-11-10 11:07:20 +0200

    styles

--- app.js
+++ app.js
@@ -10,9 +10,15 @@ class App {
       "#firebaseui-auth-container"
     );
     this.$authUser = document.querySelector(".auth-user");
-    this.$uploadButton = document.querySelector(".upload-button");
+    this.$uploadButton = document.querySelector("#create");
     this.$uploadPage = document.querySelector("#upload-page");
     this.$uploadPage.style.display = "none";
+    this.$postForm = document.querySelector(".post-form");
+    this.$closeModalBtn = document.querySelector(".close-modal-btn");
+    this.$profileBtn = document.querySelector(".profile-btn");
+    this.$profileContent = document.querySelector(".profile-content");
+    this.$Content = document.querySelector(".content");
+    this.$homeBtn = document.querySelector(".Home-btn");
 
     this.ui = new firebaseui.auth.AuthUI(firebase.auth());
     this.handleAuth();
@@ -27,6 +33,25 @@ class App {
 
     this.saveToStorage();
     // this.readFromStorage();
+
+    this.$uploadPage.addEventListener("click", this.closeModal.bind(this));
+    this.$closeModalBtn.addEventListener("click", this.closeModal.bind(this));
+
+    this.$postForm.addEventListener("click", function (event) {
+      event.stopPropagation();
+    });
+
+    this.$postForm.addEventListener("click", (e) => {
+      this.modalOpened();
+    });
+
+    this.$profileBtn.addEventListener("click", (e) => {
+      this.toggleContent();
+    });
+
+    this.$homeBtn.addEventListener("click", (e) => {
+      this.ReturnHome();
+    });
   }
 
   saveToStorage() {
@@ -118,77 +143,100 @@ class App {
 
           const postDiv = document.createElement("div");
           postDiv.classList.add("post");
+          document.querySelector(".username").textContent = post.displayName
+            .toLowerCase()
+            .replace(/\s+/g, ""); // example: "akhilboddu"
+          document.querySelector(".name").textContent = post.displayName;
 
           // Add the inner HTML for the post
           postDiv.innerHTML = `
-                  <div class="header">
-                    <div class="profile-area">
-                      <div class="post-pic">
-                        <img
-                          alt="${post.displayName}'s profile picture"
-                          class="_6q-tv"
-                          data-testid="user-avatar"
-                          draggable="false"
-                          src="assets/akhil.png"
-                        />
-                      </div>
-                      <span class="profile-name">${post.displayName}</span>
-                    </div>
-                    <div class="options">
-                      <div class="Igw0E rBNOH YBx95 _4EzTm" style="height: 24px; width: 24px">
-                        <svg aria-label="More options" class="_8-yf5" fill="#262626" height="16" viewBox="0 0 48 48" width="16">
-                          <circle clip-rule="evenodd" cx="8" cy="24" fill-rule="evenodd" r="4.5"></circle>
-                          <circle clip-rule="evenodd" cx="24" cy="24" fill-rule="evenodd" r="4.5"></circle>
-                          <circle clip-rule="evenodd" cx="40" cy="24" fill-rule="evenodd" r="4.5"></circle>
-                        </svg>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="dynamic-image">
-                    <img src="${post.url}" alt="Dynamic Image">
-                  </div>
-                  <div class="footer">
-                    <div class="user-actions">
-                      <div class="like-comment-share">
-                        <div>
-                          <span>
-                            <svg aria-label="Like" class="_8-yf5" fill="#262626" height="24" viewBox="0 0 48 48" width="24">
-                              <path d="M34.6 6.1c5.7 0 10.4 5.2 10.4 11.5 0 6.8-5.9 11-11.5 16S25 41.3 24 41.9c-1.1-.7-4.7-4-9.5-8.3-5.7-5-11.5-9.2-11.5-16C3 11.3 7.7 6.1 13.4 6.1c4.2 0 6.5 2 8.1 4.3 1.9 2.6 2.2 3.9 2.5 3.9.3 0 .6-1.3 2.5-3.9 1.6-2.3 3.9-4.3 8.1-4.3m0-3c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5.6 0 1.1-.2 1.6-.5 1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path>
-                            </svg>
-                          </span>
-                        </div>
-                        <div class="margin-left-small">
-                          <svg aria-label="Comment" class="_8-yf5" fill="#262626" height="24" viewBox="0 0 48 48" width="24">
-                            <path clip-rule="evenodd" d="M47.5 46.1l-2.8-11c1.8-3.3 2.8-7.1 2.8-11.1C47.5 11 37 .5 24 .5S.5 11 .5 24 11 47.5 24 47.5c4 0 7.8-1 11.1-2.8l11 2.8c.8.2 1.6-.6 1.4-1.4zm-3-22.1c0 4-1 7-2.6 10-.2.4-.3.9-.2 1.4l2.1 8.4-8.3-2.1c-.5-.1-1-.1-1.4.2-1.8 1-5.2 2.6-10 2.6-11.4 0-20.6-9.2-20.6-20.5S12.7 3.5 24 3.5 44.5 12.7 44.5 24z" fill-rule="evenodd"></path>
-                          </svg>
-                        </div>
-                        <div class="margin-left-small">
-                          <svg aria-label="Share Post" class="_8-yf5" fill="#262626" height="24" viewBox="0 0 48 48" width="24">
-                            <path d="M47.8 3.8c-.3-.5-.8-.8-1.3-.8h-45C.9 3.1.3 3.5.1 4S0 5.2.4 5.7l15.9 15.6 5.5 22.6c.1.6.6 1 1.2 1.1h.2c.5 0 1-.3 1.3-.7l23.2-39c.4-.4.4-1 .1-1.5zM5.2 6.1h35.5L18 18.7 5.2 6.1zm18.7 33.6l-4.4-18.4L42.4 8.6 23.9 39.7z"></path>
-                          </svg>
-                        </div>
-                      </div>
-                      <div class="bookmark">
-                        <div class="QBdPU rrUvL">
-                          <svg aria-label="Save" class="_8-yf5" fill="#262626" height="24" viewBox="0 0 48 48" width="24">
-                            <path d="M43.5 48c-.4 0-.8-.2-1.1-.4L24 29 5.6 47.6c-.4.4-1.1.6-1.6.3-.6-.2-1-.8-1-1.4v-45C3 .7 3.7 0 4.5 0h39c.8 0 1.5.7 1.5 1.5v45c0 .6-.4 1.2-.9 1.4-.2.1-.4.1-.6.1zM24 26c.8 0 1.6.3 2.2.9l15.8 16V3H6v39.9l15.8-16c.6-.6 1.4-.9 2.2-.9z"></path>
-                          </svg>
-                        </div>
-                      </div>
-                    </div>
-                    <span class="likes">Liked by <b>${
-                      post.likes || "no one"
-                    }</b> and <b>others</b></span>
-                    <div class="caption">
-                      <div class="dynamic-caption">
-                        <span class="caption-username"><b>${
-                          post.displayName
-                        }</b> ${post.caption}</span>
-                      </div>
-                    </div>
-                    <span class="timestamp">1 hour ago</span>
-                  </div>
-                `;
+  <div class="post">
+    <div class="header">
+      <div class="profile-area">
+        <div class="post-pic">
+          <img
+            alt="${post.displayName}'s profile picture"
+            class="_6q-tv"
+            data-testid="user-avatar"
+            draggable="false"
+            src="assets/akhil.png"
+          />
+        </div>
+        <span class="profile-name">${post.displayName}</span>
+      </div>
+      <div class="options">
+        <div class="Igw0E rBNOH YBx95 _4EzTm" style="height: 24px; width: 24px">
+          <svg aria-label="More options" class="_8-yf5" fill="#262626" height="16" viewBox="0 0 48 48" width="16">
+            <circle clip-rule="evenodd" cx="8" cy="24" fill-rule="evenodd" r="4.5"></circle>
+            <circle clip-rule="evenodd" cx="24" cy="24" fill-rule="evenodd" r="4.5"></circle>
+            <circle clip-rule="evenodd" cx="40" cy="24" fill-rule="evenodd" r="4.5"></circle>
+          </svg>
+        </div>
+      </div>
+    </div>
+    <div class="body">
+      <img
+        alt="Photo by ${post.displayName}"
+        class="FFVAD"
+        decoding="auto"
+        sizes="614px"
+        src="${post.url}"
+        style="object-fit: cover"
+      />
+    </div>
+    <div class="footer">
+      <div class="user-actions">
+        <div class="like-comment-share">
+          <div>
+            <span>
+              <svg aria-label="Like" class="_8-yf5" fill="#262626" height="24" viewBox="0 0 48 48" width="24">
+               <path
+                                d="M34.6 6.1c5.7 0 10.4 5.2 10.4 11.5 0 6.8-5.9 11-11.5 16S25 41.3 24 41.9c-1.1-.7-4.7-4-9.5-8.3-5.7-5-11.5-9.2-11.5-16C3 11.3 7.7 6.1 13.4 6.1c4.2 0 6.5 2 8.1 4.3 1.9 2.6 2.2 3.9 2.5 3.9.3 0 .6-1.3 2.5-3.9 1.6-2.3 3.9-4.3 8.1-4.3m0-3c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5.6 0 1.1-.2 1.6-.5 1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"
+                              ></path>
+              </svg>
+            </span>
+          </div>
+          <div class="margin-left-small">
+            <svg aria-label="Comment" class="_8-yf5" fill="#262626" height="24" viewBox="0 0 48 48" width="24">
+             <path
+                              clip-rule="evenodd"
+                              d="M47.5 46.1l-2.8-11c1.8-3.3 2.8-7.1 2.8-11.1C47.5 11 37 .5 24 .5S.5 11 .5 24 11 47.5 24 47.5c4 0 7.8-1 11.1-2.8l11 2.8c.8.2 1.6-.6 1.4-1.4zm-3-22.1c0 4-1 7-2.6 10-.2.4-.3.9-.2 1.4l2.1 8.4-8.3-2.1c-.5-.1-1-.1-1.4.2-1.8 1-5.2 2.6-10 2.6-11.4 0-20.6-9.2-20.6-20.5S12.7 3.5 24 3.5 44.5 12.7 44.5 24z"
+                              fill-rule="evenodd"
+                            ></path>
+            </svg>
+          </div>
+          <div class="margin-left-small">
+            <svg aria-label="Share Post" class="_8-yf5" fill="#262626" height="24" viewBox="0 0 48 48" width="24">
+<path
+                              d="M47.8 3.8c-.3-.5-.8-.8-1.3-.8h-45C.9 3.1.3 3.5.1 4S0 5.2.4 5.7l15.9 15.6 5.5 22.6c.1.6.6 1 1.2 1.1h.2c.5 0 1-.3 1.3-.7l23.2-39c.4-.4.4-1 .1-1.5zM5.2 6.1h35.5L18 18.7 5.2 6.1zm18.7 33.6l-4.4-18.4L42.4 8.6 23.9 39.7z"
+                            ></path>
+            </svg>
+          </div>
+        </div>
+        <div class="bookmark">
+          <div class="QBdPU rrUvL">
+            <svg aria-label="Save" class="_8-yf5" fill="#262626" height="24" viewBox="0 0 48 48" width="24">
+              <path
+                              d="M43.5 48c-.4 0-.8-.2-1.1-.4L24 29 5.6 47.6c-.4.4-1.1.6-1.6.3-.6-.2-1-.8-1-1.4v-45C3 .7 3.7 0 4.5 0h39c.8 0 1.5.7 1.5 1.5v45c0 .6-.4 1.2-.9 1.4-.2.1-.4.1-.6.1zM24 26c.8 0 1.6.3 2.2.9l15.8 16V3H6v39.9l15.8-16c.6-.6 1.4-.9 2.2-.9z"
+                            ></path>
+            </svg>
+          </div>
+        </div>
+      </div>
+      <span class="likes">
+        Liked by <b>ishitaaaa.b</b> and <b>others</b>
+      </span>
+      <span class="caption">
+        <span class="caption-username"><b>${post.displayName}</b></span>
+        <span class="caption-text">${post.caption}</span>
+      </span>
+      <span class="posted-time">5 hours ago</span>
+    </div>
+    <div class="add-comment">
+      <input type="text" placeholder="Add a comment..." />
+      <a class="post-btn">Post</a>
+    </div>
+  </div>`;
 
           // Append the post div to the dynamic content area
           dynamicContentDiv.appendChild(postDiv);
@@ -201,7 +249,31 @@ class App {
 
   handleUpload() {
     this.$firebaseAuthContainer.style.display = "none";
-    this.$app.style.display = "none";
+    this.$app.style.display = "block";
+    this.$uploadPage.style.display = "block";
+    // this.$uploadPage.classList.remove("hidden");
+    // this.$uploadPage.classList.add("show");
+  }
+
+  closeModal(event) {
+    if (
+      !this.$postForm.contains(event.target) ||
+      this.$closeModalBtn.contains(event.target)
+    ) {
+      this.$firebaseAuthContainer.style.display = "none";
+      this.$app.style.display = "block";
+      this.$uploadPage.style.display = "none";
+    }
+  }
+
+  modalOpened() {
+    if (this.$postForm) {
+      console.log("Post form element found");
+    } else {
+      console.log("Post form element not found");
+    }
+    this.$firebaseAuthContainer.style.display = "none";
+    this.$app.style.display = "block";
     this.$uploadPage.style.display = "block";
   }
 
@@ -242,6 +314,20 @@ class App {
       signInOptions: [firebase.auth.EmailAuthProvider.PROVIDER_ID],
       // Other config options...
     });
+  }
+
+  toggleContent() {
+    if ((this.$Content.style.display = "block")) {
+      this.$Content.style.display = "none";
+      this.$profileContent.style.display = "block";
+    }
+  }
+
+  ReturnHome() {
+    if ((this.$Content.style.display = "none")) {
+      this.$Content.style.display = "block";
+      this.$profileContent.style.display = "none";
+    }
   }
 }
 
